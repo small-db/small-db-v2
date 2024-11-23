84809419fdd5f48abe6982c7626c2572319a2ee7
XiaochenCui <jcnlcxc.new@gmail.com>
2024-11-22 19:14:00 -0800

    update

--- src/store/store.cc
+++ src/store/store.cc
@@ -3,6 +3,12 @@
 #include <sys/stat.h> // For mkdir
 #include <unistd.h>   // For chdir
 
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/arrow/writer.h>
+#include <parquet/exception.h>
+
 using namespace std;
 
 namespace store {
@@ -11,7 +17,31 @@ const string DATA_DIR = "data/";
 
 const string CATALOGS_FILE = "catalogs.parquet";
 
-void init_system_databases() {}
+void init_system_databases() {
+  auto type_tables = arrow::map(arrow::utf8(), arrow::uint64());
+  std::shared_ptr<arrow::Schema> schema =
+      arrow::schema({arrow::field("name", arrow::utf8()),
+                     arrow::field("tables", type_tables)});
+
+  arrow::StringBuilder schema_names_builder;
+  PARQUET_THROW_NOT_OK(schema_names_builder.Append("some"));
+  PARQUET_THROW_NOT_OK(schema_names_builder.Append("string"));
+  PARQUET_THROW_NOT_OK(schema_names_builder.Append("content"));
+  PARQUET_THROW_NOT_OK(schema_names_builder.Append("in"));
+  PARQUET_THROW_NOT_OK(schema_names_builder.Append("rows"));
+  std::shared_ptr<arrow::Array> schema_names;
+  PARQUET_THROW_NOT_OK(schema_names_builder.Finish(&schema_names));
+
+  arrow::StructBuilder tables_builder(type_tables, NULL, NULL);
+  PARQUET_THROW_NOT_OK(tables_builder.Append("some"));
+
+  return arrow::Table::Make(schema, {schema_names, strarray});
+
+  // auto table_name = arrow::Field("name", arrow::utf8());
+  // auto table_id = arrow::Field("id", arrow::uint64());
+  // auto tables =
+  //     arrow::Field("tables", arrow::map(arrow::utf8(), arrow::uint64()));
+}
 
 void init_default_database() {}
 
--- src/tmp/parquet_rw.cc
+++ src/tmp/parquet_rw.cc
@@ -27,31 +27,6 @@
 // To have some input data, we first create an Arrow Table that holds
 // some data.
 std::shared_ptr<arrow::Table> generate_table() {
-  auto type_tables = arrow::map(arrow::utf8(), arrow::uint64());
-  std::shared_ptr<arrow::Schema> schema =
-      arrow::schema({arrow::field("name", arrow::utf8()),
-                     arrow::field("tables", type_tables)});
-
-  arrow::StringBuilder schema_names_builder;
-  PARQUET_THROW_NOT_OK(schema_names_builder.Append("some"));
-  PARQUET_THROW_NOT_OK(schema_names_builder.Append("string"));
-  PARQUET_THROW_NOT_OK(schema_names_builder.Append("content"));
-  PARQUET_THROW_NOT_OK(schema_names_builder.Append("in"));
-  PARQUET_THROW_NOT_OK(schema_names_builder.Append("rows"));
-  std::shared_ptr<arrow::Array> schema_names;
-  PARQUET_THROW_NOT_OK(schema_names_builder.Finish(&schema_names));
-
-  arrow::StructBuilder tables_builder(type_tables, NULL, NULL);
-  PARQUET_THROW_NOT_OK(tables_builder.Append("some"));
-
-  return arrow::Table::Make(schema, {schema_names, strarray});
-
-  auto table_name = arrow::Field("name", arrow::utf8());
-  auto table_id = arrow::Field("id", arrow::uint64());
-  auto tables =
-      arrow::Field("tables", arrow::map(arrow::utf8(), arrow::uint64()));
-
-  // arrow::StructBuilder structbuilder()
 
   arrow::Int64Builder i64builder;
   PARQUET_THROW_NOT_OK(i64builder.AppendValues({1, 2, 3, 4, 5}));
