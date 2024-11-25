451badc2a0efba2540d832878255e9efe2165619
XiaochenCui <jcnlcxc.new@gmail.com>
2024-11-25 06:45:42 -0800

    update

--- src/store/store.cc
+++ src/store/store.cc
@@ -80,55 +80,61 @@ void init_schemas() {
 }
 
 void init_tables() {
-  // schema for "database tables"
-  auto type_columns =
-      arrow::list(arrow::struct_({arrow::field("name", arrow::utf8()),
-                                  arrow::field("type", arrow::uint8())}));
-  std::shared_ptr<arrow::Schema> schema =
-      arrow::schema({arrow::field("name", arrow::utf8()),
-                     arrow::field("columns", type_columns)});
+  auto pool = arrow::default_memory_pool();
 
-  // schema.names for "database tables"
-  auto table_names_builder = arrow::StringBuilder();
-  PARQUET_THROW_NOT_OK(table_names_builder.Append("pg_database"));
-  std::shared_ptr<arrow::Array> table_names;
-  PARQUET_THROW_NOT_OK(table_names_builder.Finish(&table_names));
+  // declare custom types
+  auto type_column_name = arrow::utf8();
+  auto type_column_type = arrow::uint8();
+  auto type_column = arrow::struct_({arrow::field("name", type_column_name),
+                                     arrow::field("type", type_column_type)});
+  auto type_columns = arrow::list(type_column);
 
-  // schema.columns for "database tables"
+  // names
+  auto table_name_builder = arrow::StringBuilder();
+  PARQUET_THROW_NOT_OK(table_name_builder.Append("pg_database"));
+  shared_ptr<arrow::Array> table_names;
+  PARQUET_THROW_NOT_OK(table_name_builder.Finish(&table_names));
+
+  // columns
   auto column_names_builder = std::make_shared<arrow::StringBuilder>();
   auto column_types_builder = std::make_shared<arrow::UInt8Builder>();
-  shared_ptr<arrow::ArrayBuilder> field_builder =
-      std::make_shared<arrow::StructBuilder>(
-          arrow::struct_({arrow::field("name", arrow::utf8()),
-                          arrow::field("type", arrow::uint8())}));
-  arrow::ListBuilder columns_builder(arrow::default_memory_pool(),
-                                     field_builder, type_columns);
+  std::vector<std::shared_ptr<arrow::ArrayBuilder>> field_builders = {
+      column_names_builder, column_types_builder};
+  auto column_builder =
+      make_shared<arrow::StructBuilder>(type_column, pool, field_builders);
+  auto columns_builder = make_shared<arrow::ListBuilder>(pool, column_builder);
+
+  // columns for "pg_database"
+  PARQUET_THROW_NOT_OK(columns_builder->Append());
 
   // column "datname"
-  PARQUET_THROW_NOT_OK(columns_builder.Append());
+  PARQUET_THROW_NOT_OK(column_builder->Append());
   PARQUET_THROW_NOT_OK(column_names_builder->Append("datname"));
   PARQUET_THROW_NOT_OK(column_types_builder->Append(TYPE_STRING));
 
   // column "datcollate"
-  PARQUET_THROW_NOT_OK(columns_builder.Append());
+  PARQUET_THROW_NOT_OK(column_builder->Append());
   PARQUET_THROW_NOT_OK(column_names_builder->Append("datcollate"));
   PARQUET_THROW_NOT_OK(column_types_builder->Append(TYPE_STRING));
 
   // column "datctype"
-  PARQUET_THROW_NOT_OK(columns_builder.Append());
+  PARQUET_THROW_NOT_OK(column_builder->Append());
   PARQUET_THROW_NOT_OK(column_names_builder->Append("datctype"));
   PARQUET_THROW_NOT_OK(column_types_builder->Append(TYPE_STRING));
 
   // column "datacl"
-  PARQUET_THROW_NOT_OK(columns_builder.Append());
+  PARQUET_THROW_NOT_OK(column_builder->Append());
   PARQUET_THROW_NOT_OK(column_names_builder->Append("datacl"));
   PARQUET_THROW_NOT_OK(column_types_builder->Append(TYPE_STRING));
 
-  std::shared_ptr<arrow::Array> columns;
-  PARQUET_THROW_NOT_OK(columns_builder.Finish(&columns));
+  std::shared_ptr<arrow::Array> columns_list;
+  PARQUET_THROW_NOT_OK(columns_builder->Finish(&columns_list));
 
   // create the table
-  auto table = arrow::Table::Make(schema, {table_names, columns});
+  std::shared_ptr<arrow::Schema> schema =
+      arrow::schema({arrow::field("name", arrow::utf8()),
+                     arrow::field("columns", type_columns)});
+  auto table = arrow::Table::Make(schema, {table_names, columns_list});
   SPDLOG_INFO("generated table:\n{}", table->ToString());
 
   // write to disk
@@ -137,22 +143,6 @@ void init_tables() {
                                        gen_datafile_path(TABLE_TABLES)));
   PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(
       *table, arrow::default_memory_pool(), outfile, 300));
-}
-
-void read_whole_file() {
-  SPDLOG_INFO("reading {} at once", TABLE_SCHEMAS);
-  std::shared_ptr<arrow::io::ReadableFile> infile;
-  PARQUET_ASSIGN_OR_THROW(
-      infile, arrow::io::ReadableFile::Open(TABLE_SCHEMAS,
-                                            arrow::default_memory_pool()));
-
-  std::unique_ptr<parquet::arrow::FileReader> reader;
-  PARQUET_THROW_NOT_OK(
-      parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
-  std::shared_ptr<arrow::Table> table;
-  PARQUET_THROW_NOT_OK(reader->ReadTable(&table));
-  SPDLOG_INFO("loaded {} rows in {} columns", table->num_rows(),
-              table->num_columns());
 }
 
 void init_default_database() {}
