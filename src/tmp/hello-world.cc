9d44113d3b20cf9a49f69099ea15482b59f724b5
XiaochenCui <jcnlcxc.new@gmail.com>
2024-12-17 14:22:01 -0800

    update

--- src/tmp/hello-world.cc
+++ src/tmp/hello-world.cc
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <optional>
+#include <ranges>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // test features from C++17
+    std::optional<int> opt;
+
+    // // test features from C++20
+    // std::ranges::iota_view<int> range(0, 10);
+
     return 0;
 }
