36ae8a6d3388641a44f8de67cde9e043f5772395
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-02 12:27:34 -0800

    update

--- src/tmp/hello_world.cc
+++ src/tmp/hello_world.cc
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <thread>
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+
+    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
