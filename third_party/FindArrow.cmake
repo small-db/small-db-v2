d0bc4e976d067d5803f730fb7ad4565bf54408a3
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-04 20:22:50 -0800

    update

--- third_party/FindArrow.cmake
+++ third_party/FindArrow.cmake
@@ -15,9 +15,28 @@ include(FetchContent)
 
 FetchContent_Declare(Arrow
   GIT_REPOSITORY https://github.com/apache/arrow.git
-    GIT_TAG apache-arrow-19.0.1
-  GIT_SHALLOW TRUE SOURCE_SUBDIR cpp
+  GIT_TAG apache-arrow-19.0.1
+  GIT_SHALLOW TRUE
+  SOURCE_SUBDIR cpp
   OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_MakeAvailable(Arrow)
+
+message(STATUS "arrow_BINARY_DIR: ${arrow_BINARY_DIR}")
+message(STATUS "arrow_SOURCE_DIR: ${arrow_SOURCE_DIR}")
+
+add_library(Arrow::arrow_static INTERFACE IMPORTED)
+target_link_libraries(Arrow::arrow_static INTERFACE arrow_static)
+target_include_directories(Arrow::arrow_static
+  INTERFACE ${arrow_BINARY_DIR}/src
+  ${arrow_SOURCE_DIR}/cpp/src)
+
+# message(STATUS "INCLUDE_DIRECTORIES: ${INCLUDE_DIRECTORIES}")
+# message(STATUS "Arrow_INCLUDE_DIRS: ${Arrow_INCLUDE_DIRS}")
+# message(STATUS "CMAKE_CURRENT_BINARY_DIR: ${CMAKE_CURRENT_BINARY_DIR}")
+
+# include_directories(${Arrow_INCLUDE_DIRS})
+
+# message(STATUS "INCLUDE_DIRECTORIES: ${INCLUDE_DIRECTORIES}")
+# message(STATUS "Arrow_INCLUDE_DIRS: ${Arrow_INCLUDE_DIRS}")
