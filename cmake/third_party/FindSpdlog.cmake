a92f4b92bfd1902770e01b1b7f25489d67fe1b4b
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-07 14:57:24 -0800

    update

--- cmake/third_party/FindSpdlog.cmake
+++ cmake/third_party/FindSpdlog.cmake
@@ -1,11 +1,7 @@
 FetchContent_Declare(
-    spdlog 
+    spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
     GIT_TAG v1.15.1
 )
 
-message(STATUS "spdlog_BINARY_DIR: ${spdlog_BINARY_DIR}")
-message(STATUS "spdlog_SOURCE_DIR: ${spdlog_SOURCE_DIR}")
-
-# list(APPEND Spdlog_INCLUDE_DIRS ${spdlog_SOURCE_DIR}/cpp/src)
 list(APPEND Spdlog_INCLUDE_DIRS ${spdlog_SOURCE_DIR}/include)
