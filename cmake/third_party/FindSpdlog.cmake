85ec06dcbc3f819b318c1a47c203cd8e0b51cbea
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-25 08:43:15 -0700

    update

--- cmake/third_party/FindSpdlog.cmake
+++ cmake/third_party/FindSpdlog.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
-    spdlog
+    spdlog_abc
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
     GIT_TAG v1.15.1
 )
 
-FetchContent_MakeAvailable(spdlog)
+FetchContent_MakeAvailable(spdlog_abc)
