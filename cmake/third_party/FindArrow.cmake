32187d701e4d0668e069fa8b4486be49f659cea1
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-28 13:38:49 -0700

    update

--- cmake/third_party/FindArrow.cmake
+++ cmake/third_party/FindArrow.cmake
@@ -2,7 +2,9 @@
 # - https://github.com/heavyai/heavydb/blob/a5dc49c757739d87f12baf8038ccfe4d1ece88ea/cmake/Modules/FindArrow.cmake
 # - https://github.com/amoeba/arrow-cmake-fetchcontent/blob/main/CMakeLists.txt
 
-set(ARROW_BUILD_SHARED True)
+# set(ARROW_BUILD_SHARED True)
+# set(ARROW_BUILD_SHARED False)
+set(ARROW_BUILD_STATIC On)
 set(ARROW_DEPENDENCY_SOURCE "BUNDLED")
 set(ARROW_SIMD_LEVEL NONE)
 
@@ -23,32 +25,57 @@ FetchContent_Declare(Arrow
   SOURCE_SUBDIR cpp
 )
 
+get_property(targets DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY BUILDSYSTEM_TARGETS)
+
+message(STATUS "Available targets:")
+foreach(target IN LISTS targets)
+  message(STATUS " - ${target}")
+endforeach()
+
 FetchContent_MakeAvailable(Arrow)
 
-list(APPEND Arrow_INCLUDE_DIRS ${arrow_SOURCE_DIR}/cpp/src)
-list(APPEND Arrow_INCLUDE_DIRS ${arrow_BINARY_DIR}/src)
+# get_property(targets DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY BUILDSYSTEM_TARGETS)
+# get_property(targets GLOBAL PROPERTY BUILDSYSTEM_TARGETS)
+get_property(targets GLOBAL PROPERTY IMPORTED_TARGETS)
 
-add_library(arrow_lib INTERFACE IMPORTED)
+message(STATUS "targets: ${targets}")
 
-target_link_libraries(
-  arrow_lib
-  INTERFACE
-  arrow
-  arrow_dataset
-  arrow_acero
-  parquet
-)
+message(STATUS "Available targets:")
+foreach(target IN LISTS targets)
+  message(STATUS " - ${target}")
+endforeach()
 
-target_include_directories(
-  arrow_lib
-  INTERFACE
-  ${arrow_BINARY_DIR}/src
-  ${arrow_SOURCE_DIR}/cpp/src
-)
+get_property(targets DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY IMPORTED_TARGETS)
+message(STATUS "targets: ${targets}")
 
-target_link_directories(
-  arrow_lib
-  INTERFACE
-  ${arrow_BINARY_DIR}/debug
-)
+get_property(all_targets DIRECTORY PROPERTY BUILDSYSTEM_TARGETS)
+message(STATUS "all_targets: ${all_targets}")
+
+
+# list(APPEND Arrow_INCLUDE_DIRS ${arrow_SOURCE_DIR}/cpp/src)
+# list(APPEND Arrow_INCLUDE_DIRS ${arrow_BINARY_DIR}/src)
+
+# add_library(arrow_lib INTERFACE IMPORTED)
+
+# target_link_libraries(
+#   arrow_lib
+#   INTERFACE
+#   arrow
+#   arrow_dataset
+#   arrow_acero
+#   parquet
+# )
+
+# target_include_directories(
+#   arrow_lib
+#   INTERFACE
+#   ${arrow_BINARY_DIR}/src
+#   ${arrow_SOURCE_DIR}/cpp/src
+# )
+
+# target_link_directories(
+#   arrow_lib
+#   INTERFACE
+#   ${arrow_BINARY_DIR}/debug
+# )
 
