93fa0adfa63e531ecd343dd431faa19acb1d9a83
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-30 15:33:00 -0700

    update

--- cmake/third_party/FindArrow.cmake
+++ cmake/third_party/FindArrow.cmake
@@ -2,26 +2,24 @@
 # - https://github.com/heavyai/heavydb/blob/a5dc49c757739d87f12baf8038ccfe4d1ece88ea/cmake/Modules/FindArrow.cmake
 # - https://github.com/amoeba/arrow-cmake-fetchcontent/blob/main/CMakeLists.txt
 
-block()
-  set(ARROW_BUILD_STATIC ON)
-  set(ARROW_DEPENDENCY_SOURCE "BUNDLED")
-  set(ARROW_SIMD_LEVEL NONE)
-  set(ARROW_ACERO ON)
-  set(ARROW_PARQUET ON)
-  set(ARROW_IPC ON)
-  set(ARROW_DATASET ON)
-  set(ARROW_FILESYSTEM ON)
-  set(ARROW_COMPUTE ON)
-  set(ARROW_BUILD_TESTS OFF)
+set(ARROW_BUILD_STATIC ON)
+set(ARROW_DEPENDENCY_SOURCE "BUNDLED")
+set(ARROW_SIMD_LEVEL NONE)
+set(ARROW_ACERO ON)
+set(ARROW_PARQUET ON)
+set(ARROW_IPC ON)
+set(ARROW_DATASET ON)
+set(ARROW_FILESYSTEM ON)
+set(ARROW_COMPUTE ON)
+set(ARROW_BUILD_TESTS OFF)
 
-  FetchContent_Declare(Arrow
-    GIT_REPOSITORY https://github.com/apache/arrow.git
-    GIT_TAG apache-arrow-19.0.1
-    GIT_SHALLOW TRUE
-    SOURCE_SUBDIR cpp
-  )
-  FetchContent_MakeAvailable(Arrow)
-endblock()
+FetchContent_Declare(Arrow
+  GIT_REPOSITORY https://github.com/apache/arrow.git
+  GIT_TAG apache-arrow-19.0.1
+  GIT_SHALLOW TRUE
+  SOURCE_SUBDIR cpp
+)
+FetchContent_MakeAvailable(Arrow)
 
 add_library(arrow_lib INTERFACE IMPORTED)
 
