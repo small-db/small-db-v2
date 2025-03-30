9d81bbefe63cbfc76103a1318d28902997e5e609
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-30 15:50:34 -0700

    update

--- cmake/third_party/FindArrow.cmake
+++ cmake/third_party/FindArrow.cmake
@@ -2,52 +2,55 @@
 # - https://github.com/heavyai/heavydb/blob/a5dc49c757739d87f12baf8038ccfe4d1ece88ea/cmake/Modules/FindArrow.cmake
 # - https://github.com/amoeba/arrow-cmake-fetchcontent/blob/main/CMakeLists.txt
 
-set(ARROW_BUILD_STATIC ON)
-set(ARROW_DEPENDENCY_SOURCE "BUNDLED")
-set(ARROW_SIMD_LEVEL NONE)
-set(ARROW_ACERO ON CACHE BOOL "acero")
-set(ARROW_PARQUET ON)
-set(ARROW_IPC ON)
-set(ARROW_DATASET ON)
-set(ARROW_FILESYSTEM ON)
-set(ARROW_COMPUTE ON)
-set(ARROW_BUILD_TESTS OFF)
+block()
 
-FetchContent_Declare(Arrow
-  GIT_REPOSITORY https://github.com/apache/arrow.git
-  GIT_TAG apache-arrow-19.0.1
-  GIT_SHALLOW TRUE
-  SOURCE_SUBDIR cpp
-)
+  set(ARROW_BUILD_STATIC ON)
+  set(ARROW_DEPENDENCY_SOURCE "BUNDLED")
+  set(ARROW_SIMD_LEVEL NONE)
+  set(ARROW_ACERO ON CACHE BOOL "acero")
+  set(ARROW_PARQUET ON)
+  set(ARROW_IPC ON)
+  set(ARROW_DATASET ON)
+  set(ARROW_FILESYSTEM ON)
+  set(ARROW_COMPUTE ON)
+  set(ARROW_BUILD_TESTS OFF)
 
-get_all_targets(. BEFORE_TARGETS)
+  FetchContent_Declare(Arrow
+    GIT_REPOSITORY https://github.com/apache/arrow.git
+    GIT_TAG apache-arrow-19.0.1
+    GIT_SHALLOW TRUE
+    SOURCE_SUBDIR cpp
+  )
 
-FetchContent_MakeAvailable(Arrow)
+  get_all_targets(. BEFORE_TARGETS)
 
-get_all_targets(. AFTER_TARGETS)
-print_added_target(BEFORE_TARGETS AFTER_TARGETS)
+  FetchContent_MakeAvailable(Arrow)
 
-add_library(arrow_lib INTERFACE IMPORTED)
+  get_all_targets(. AFTER_TARGETS)
+  print_added_target(BEFORE_TARGETS AFTER_TARGETS)
 
-target_link_libraries(
-  arrow_lib
-  INTERFACE
-  arrow_static
-  arrow_dataset_static
-  arrow_acero_static
-  parquet_static
-)
+  add_library(arrow_lib INTERFACE IMPORTED)
 
-target_include_directories(
-  arrow_lib
-  INTERFACE
-  ${arrow_BINARY_DIR}/src
-  ${arrow_SOURCE_DIR}/cpp/src
-)
+  target_link_libraries(
+    arrow_lib
+    INTERFACE
+    arrow_static
+    arrow_dataset_static
+    arrow_acero_static
+    parquet_static
+  )
 
-target_link_directories(
-  arrow_lib
-  INTERFACE
-  ${arrow_BINARY_DIR}/debug
-)
+  target_include_directories(
+    arrow_lib
+    INTERFACE
+    ${arrow_BINARY_DIR}/src
+    ${arrow_SOURCE_DIR}/cpp/src
+  )
 
+  target_link_directories(
+    arrow_lib
+    INTERFACE
+    ${arrow_BINARY_DIR}/debug
+  )
+
+endblock()
