69eda689f454cb25a03ac779ab4ea4ca84ca91d9
XiaochenCui <jcnlcxc.new@gmail.com>
2025-03-30 14:51:23 -0700

    update

--- cmake/third_party/FindRocksDB.cmake
+++ cmake/third_party/FindRocksDB.cmake
@@ -1,17 +1,13 @@
-set(WITH_GFLAGS OFF)
-set(ROCKSDB_BUILD_SHARED OFF)
-set(WITH_TESTS OFF)
-set(WITH_BENCHMARK_TOOLS OFF)
+block()
+  set(WITH_GFLAGS OFF CACHE BOOL "gflags")
+  set(ROCKSDB_BUILD_SHARED OFF CACHE BOOL "rocksdb build shared")
+  set(WITH_TESTS OFF CACHE BOOL "rocksdb without tests")
+  set(WITH_BENCHMARK_TOOLS OFF CACHE BOOL "rocksdb without benchmarking")
 
-FetchContent_Declare(RocksDB
-  GIT_REPOSITORY https://github.com/facebook/rocksdb.git
-  GIT_TAG v9.10.0
-  GIT_SHALLOW TRUE
-)
-
-get_all_targets(. BEFORE_TARGETS)
-
-FetchContent_MakeAvailable(RocksDB)
-
-get_all_targets(. AFTER_TARGETS)
-print_added_target(BEFORE_TARGETS AFTER_TARGETS)
+  FetchContent_Declare(RocksDB
+    GIT_REPOSITORY https://github.com/facebook/rocksdb.git
+    GIT_TAG v9.10.0
+    GIT_SHALLOW TRUE
+  )
+  FetchContent_MakeAvailable(RocksDB)
+endblock()
