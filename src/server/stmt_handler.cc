4df236b0a52584579c0b1ea0128a765907d58c24
XiaochenCui <jcnlcxc.new@gmail.com>
2025-04-14 13:43:53 -0700

    update

--- src/server/stmt_handler.cc
+++ src/server/stmt_handler.cc
@@ -216,7 +216,7 @@ absl::StatusOr<std::shared_ptr<arrow::RecordBatch>> handle_stmt(
             break;
         }
         case PG_QUERY__NODE__NODE_TRANSACTION_STMT: {
-            SPDLOG_ERROR("transaction statement");
+            SPDLOG_INFO("transaction statement");
             break;
         }
         case PG_QUERY__NODE__NODE_ALTER_TABLE_STMT: {
