ddf99ccab735a05594fa1266495cf4a6555a5d78
XiaochenCui <jcnlcxc.new@gmail.com>
2025-04-17 17:32:55 -0700

    update

--- src/server_registry/server_registry.cc
+++ src/server_registry/server_registry.cc
@@ -27,6 +27,7 @@
 
 // grpc
 #include "grpc/grpc.h"
+#include "grpcpp/create_channel.h"
 #include "grpcpp/server_builder.h"
 
 // spdlog
@@ -48,13 +49,21 @@
 
 namespace small::server_registry {
 
-class ServerRegService final
+class RegistryService final
     : public small::server_registry::ServerRegistry::Service {
    public:
     virtual ::grpc::Status Register(
         ::grpc::ServerContext* context,
         const ::small::server_registry::RegistryRequest* request,
-        ::small::server_registry::RegistryReply* response) {}
+        ::small::server_registry::RegistryReply* response) {
+        SPDLOG_INFO(
+            "register server: sql_address: {}, rpc_address: {}, region: {}",
+            request->sql_address(), request->rpc_address(), request->region());
+
+        response->set_success(true);
+
+        return grpc::Status::OK;
+    }
 };
 
 std::vector<std::shared_ptr<Server>> get_servers(
@@ -67,7 +76,7 @@ void start_server(int port) {
     std::string addr = fmt::format("0.0.0.0:{}", port);
     builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
 
-    ServerRegService service;
+    RegistryService service;
     builder.RegisterService(&service);
 
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
@@ -76,7 +85,29 @@ void start_server(int port) {
 }
 
 absl::Status join(std::string peer_addr) {
+    if (peer_addr.empty()) {
+        return absl::OkStatus();
+    }
+
     SPDLOG_INFO("join peer addr: {}", peer_addr);
+
+    small::server_registry::RegistryRequest request;
+    request.set_sql_address("a");
+    request.set_rpc_address("b");
+    request.set_region("c");
+
+    auto channel =
+        grpc::CreateChannel(peer_addr, grpc::InsecureChannelCredentials());
+    std::unique_ptr<small::server_registry::ServerRegistry::Stub> stub =
+        small::server_registry::ServerRegistry::NewStub(channel);
+    grpc::ClientContext context;
+    small::server_registry::RegistryReply result;
+    grpc::Status status = stub->Register(&context, request, &result);
+    if (!status.ok()) {
+        SPDLOG_ERROR("failed to join peer: {}", status.error_message());
+        return absl::InternalError(status.error_message());
+    }
+    SPDLOG_INFO("joined peer: {}", result.success());
     return absl::OkStatus();
 }
 
